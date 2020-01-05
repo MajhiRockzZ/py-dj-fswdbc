5c28a95513dfaf982ef2b138522dc8b1da3f168d
MajhiRockzZ ⚡ <sumesh.majhi.5@gmail.com>
2020-01-05 14:30:28 +0530

    feat: 🎳 javascript level 1 part 1

---
+++ js-L1/js-l1-p1.js
@@ -0,0 +1,78 @@
+// anything here is a comment
+
+// BASIC DATA TYPES
+
+10;
+20.2 - 13.4;
+
+console.log("Hello World!");
+
+// Strings
+("Hello world");
+("10");
+
+// Booleans
+true;
+false;
+
+// undefined and null
+undefined;
+null;
+
+2 + 2;
+5 - 1;
+3 * 2;
+10 / 2;
+
+2 / 5;
+
+2 ** 4;
+
+4 % 2;
+
+// STRINGS
+
+("Django is cool");
+
+"Django" + " is super cool";
+
+"django".length;
+
+"x x".length;
+
+("hello \n start a new line");
+
+("hello \t give me a tab");
+
+('hello "quotes" ');
+
+"hello"[0];
+
+"look at the dog"[12];
+
+// VARIABLES
+
+var varName = "value";
+
+var bankAccount = 100;
+
+console.log(bankAccount);
+
+var deposit = 50;
+
+var total = bankAccount + deposit;
+
+console.log(total);
+
+var greeting = "Welcome back: ";
+var name = "Sumesh";
+
+// alert(greeting + name);
+
+var myVariable; // undefined
+
+var bonus = null; // different from undefined
+
+console.log("Hey I'm in the console");
+
+// prompt("Enter something!");
