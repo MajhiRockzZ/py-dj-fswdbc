d87defefe16b9c0479da9386af194b1305a80405
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-01-29 16:04:15 +0530

    feat: add logpoints for console log

--- js-L1/js-l1-p7.js
+++ js-L1/js-l1-p7.js
@@ -1,13 +1,13 @@
 // for Loop
 
 for (var i = 0; i < 5; i++) {
-  console.log("Number is " + i);
+
 }
 
 var word = "ABCDEFGHIJK";
 
 for (var i = 0; i < word.length; i++) {
-  console.log(word[i]);
+  
 }
 
 for (var i = 0; i < word.length; i = i + 2) {
