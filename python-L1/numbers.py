9bdddba2da5abde638b52fdc7638c18396c3794b
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-02-05 23:15:17 +0530

    feat: 🐍 Python string basics

---
+++ python-L1/numbers.py
@@ -0,0 +1,13 @@
+a = 5
+print(a + a)
+a = a * 5
+
+# comment
+print(a)  # comment
+
+two_dogs = ["hera", "moti"]
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
---
+++ python-L1/strings.py
@@ -0,0 +1,13 @@
+# Basics
+'hello'
+"hello"
+
+"I'm a dog"
+
+# Indexing
+mystring = "abcdefg"
+print(mystring[0])
+
+# Slicing
+print(mystring[2:])
+print(mystring[:3])
