e9fba143960b2316a7a9d94b51ed817cb860d99f
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-02-05 23:58:25 +0530

    feat: 🐍Python string advanced

--- python-L1/strings.py
+++ python-L1/strings.py
@@ -11,3 +11,29 @@ print(mystring[0])
 # Slicing
 print(mystring[2:])
 print(mystring[:3])
+print(mystring[:])
+print(mystring[::])
+print(mystring[::2])
+
+# Basic Methods
+x = mystring.upper()
+print(x)
+x = mystring.lower()
+print(x)
+x = mystring.capitalize()
+print(x)
+mystring = "Hello World"
+x = mystring.split()
+print(x)
+x = mystring.split("e")
+print(x)
+x = mystring.split("o")
+print(x)
+
+# Print Formatting
+x = "Insert another string here: {}".format("INSERT ME!")
+print(x)
+x = "Item One: {} Item Two: {}".format("dog", "cat")
+print(x)
+x = "Item One: {y} Item Two: {x}".format(x="dog", y="cat")
+print(x)
