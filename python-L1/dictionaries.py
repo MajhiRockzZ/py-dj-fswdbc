4fcae89d9e1870254061eb5748ca6c8d6efa5b53
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-02-06 09:21:13 +0530

    feat: 🐍 Python dictionaries basic

---
+++ python-L1/dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries
+my_stuff = {"key1": "value", "key2": "value2"}
+print(my_stuff["key1"])
+my_stuff = {"key1": "123", "key2": "abc", "key3": {"123": [1, 2, "grabMe"]}}
+print(my_stuff['key3']["123"][2].upper())
+my_stuff = {"lunch": "pizza", "bfast": "eggs"}
+my_stuff["lunch"] = "burger"
+my_stuff["dinner"] = "pasta"
+print(my_stuff["lunch"])
+print(my_stuff)
