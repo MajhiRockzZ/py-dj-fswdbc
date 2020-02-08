1a83bf77f390395000780b39cc395e47d5214735
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-02-08 09:02:50 +0530

    feat: 🐍Python oop part three

--- python-L2/oop.py
+++ python-L2/oop.py
@@ -40,3 +40,67 @@ class Circle():
 my_c = Circle(4)
 my_c.set_radius(999)
 print(my_c.area)
+
+# INHERITANCE
+
+
+class Animal():
+
+    def __init__(self):
+        print("ANIMAL CREATED")
+
+    def whoAmI(self):
+        print("ANIMAL")
+
+    def eat(self):
+        print("EATING")
+
+
+mya = Animal()
+mya.whoAmI()
+mya.eat()
+
+
+class Cat(Animal):
+
+    def __init__(self):
+        Animal.__init__(self)
+        print("CAT CREATED")
+
+    def sound(self):
+        print("MEAOW")
+
+    def eat(self):
+        print("CAT EATING")
+
+
+mycat = Cat()
+mycat.whoAmI()
+mycat.eat()
+mycat.sound()
+
+
+# SPECIAL METHODS
+
+class Book():
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return "Title: {}, Author: {}, Pages: {}".format(self.title, self.author, self.pages)
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("A book")
+
+
+b = Book("Python", "Sumesh", 200)
+print(b)
+print(len(b))
+
+mylist = [1, 2, 3]
+print(mylist)
