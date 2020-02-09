9d0b44772d4356b26077fbcd7f644c833c9af4f5
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-02-09 08:55:27 +0530

    feat: 🐍 Python modules and packages

---
+++ python-L2/modules_and_packages.py
@@ -0,0 +1,11 @@
+from mymodule import *
+from mymodule import func_in_module
+import mymodule as mm
+import mymodule
+mymodule.func_in_module()
+
+mm.func_in_module()
+
+func_in_module()
+
+func_in_module()
