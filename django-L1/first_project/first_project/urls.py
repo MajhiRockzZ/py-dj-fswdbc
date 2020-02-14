5a1e3c51107c7bd69ba79a2de6dda2030109f035
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-02-14 06:07:29 +0530

    feat: 🦕 Add your own url patterns

--- django-L1/first_project/first_project/urls.py
+++ django-L1/first_project/first_project/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
 from first_app import views
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
 ]
