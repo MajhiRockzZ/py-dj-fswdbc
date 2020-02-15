867baef11cae4db89d66017ddb2de6dc55bcdb42
Sumesh Majhi ⚡ <sumesh.majhi.5@gmail.com>
2020-02-15 08:46:52 +0530

    feat: 🦕Django ProTwo Challenge

--- django-L1/ProTwo/ProTwo/urls.py
+++ django-L1/ProTwo/ProTwo/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from appTwo import views
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^help/', include('appTwo.urls')),
     path('admin/', admin.site.urls),
 ]
