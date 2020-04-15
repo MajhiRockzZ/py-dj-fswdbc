806ec5e4fa4a2cc44f58e2765e3822890d0f25b2
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-15 08:00:06 +0530

    feat: template views with cbv

--- cbv/advcbv/basic_app/views.py
+++ cbv/advcbv/basic_app/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
+# from django.http import HttpResponse
 
 # def index(request):
 #     return render(request, 'index.html')
 
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class based views are cool.")
+# class CBView(View):
+#     def get(self, request):
+#         return HttpResponse("Class based views are cool.")
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION!'
+        return context
+
+
+""" 
+*args -> Will give all function parameters as a tuple.
+
+**kwargs -> Will give all keyword arguments except for those 
+            corresponding parameter as a dictionary.
+ """
