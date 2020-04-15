5cfe9981cfaf7afeb57e37a682d8abe78d58c783
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-15 08:28:25 +0530

    feat: list view detail view part one

--- cbv/advcbv/basic_app/views.py
+++ cbv/advcbv/basic_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
 # from django.http import HttpResponse
+
+from . import models
+
+
+class SchoolListView(ListView):
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
 
 # def index(request):
 #     return render(request, 'index.html')
@@ -11,13 +23,13 @@ from django.views.generic import View, TemplateView
 #         return HttpResponse("Class based views are cool.")
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION!'
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['injectme'] = 'BASIC INJECTION!'
+#         return context
 
 
 """ 
