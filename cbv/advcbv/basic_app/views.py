aa14da5febeb0280994e00936428bca67e800214
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2020-04-17 07:27:22 +0530

    feat: django crud view

--- cbv/advcbv/basic_app/views.py
+++ cbv/advcbv/basic_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import (
+    View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 # from django.http import HttpResponse
 
 from . import models
+from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
@@ -22,22 +24,35 @@ class SchoolDetailView(DetailView):
     # school
 
 
-# def index(request):
-#     return render(request, 'index.html')
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
 
 
-# class CBView(View):
-#     def get(self, request):
-#         return HttpResponse("Class based views are cool.")
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
 
 
-# class IndexView(TemplateView):
-#     template_name = 'index.html'
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['injectme'] = 'BASIC INJECTION!'
-#         return context
+    # def index(request):
+    #     return render(request, 'index.html')
+
+    # class CBView(View):
+    #     def get(self, request):
+    #         return HttpResponse("Class based views are cool.")
+
+    # class IndexView(TemplateView):
+    #     template_name = 'index.html'
+
+    #     def get_context_data(self, **kwargs):
+    #         context = super().get_context_data(**kwargs)
+    #         context['injectme'] = 'BASIC INJECTION!'
+    #         return context
 
 
 """ 
